8a26940ba80d0de041d042af1bdedd31830b9cdb
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2024-03-29 22:20:35 -0300

    Desenvolvimento da Regra de inicialização do tabuleiro

--- GameOfLifeConsole/Entities/Table.cs
+++ GameOfLifeConsole/Entities/Table.cs
@@ -5,22 +5,48 @@
         private readonly int rows;
         private readonly int columns;
 
-        public bool[,] Cells { get; set; }
+        public Cell[] Cells { get; set; }
 
         public Table(int rowLenght, int columnLeght)
         {
             rows = rowLenght;
             columns = columnLeght;
 
-            Cells = new bool[rows, columns];
+            Cells = new Cell[rows * columns];
+
+            var arrayLenght = Cells.Length;
+
+            var aliveCells = GenerateRandomCells(10);
+
+            for (int x = 0; x < columns; x++)
+                for (int y = 0; y < rows; y++)
+                {
+                    var alive = aliveCells.Any(pos => pos.Item1 == x &&
+                                                      pos.Item2 == y);
+
+                    Cells[arrayLenght] = new Cell(x, y, alive);
+                    
+                    arrayLenght++;
+                }
+        }
+
+        private (int, int)[] GenerateRandomCells(int count)
+        {
+            var random = new Random();
+            var result = new (int, int)[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                var x = random.Next(0, rows - 1);
+                var y = random.Next(0, columns - 1);
+
+                result[i] = (x, y);
+            }
+
+            return result;
         }
 
         public (int, int)[] GetNeighboringCells()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool IsAlive(int x, int y)
         {
             throw new NotImplementedException();
         }
