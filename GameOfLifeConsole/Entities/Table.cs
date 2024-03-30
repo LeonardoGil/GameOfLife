0e744d5c4723895953199b2921e14eb5eacca6b2
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2024-03-29 21:43:19 -0300

    Gerando estrutura basica da Tabela

---
+++ GameOfLifeConsole/Entities/Table.cs
@@ -0,0 +1,28 @@
+﻿namespace GameOfLifeConsole.Entities
+{
+    internal class Table
+    {
+        private readonly int rows;
+        private readonly int columns;
+
+        public bool[,] Cells { get; set; }
+
+        public Table(int rowLenght, int columnLeght)
+        {
+            rows = rowLenght;
+            columns = columnLeght;
+
+            Cells = new bool[rows, columns];
+        }
+
+        public (int, int)[] GetNeighboringCells()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsAlive(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
