0308204691e3921c560c529f6da0fab5e74a9232
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2024-03-29 22:20:17 -0300

    Criação da estrutura da Celula

---
+++ GameOfLifeConsole/Entities/Cell.cs
@@ -0,0 +1,22 @@
+﻿namespace GameOfLifeConsole.Entities
+{
+    internal class Cell
+    {
+        public bool Alive { get; private set; }
+
+        public bool? WillLive { get; private set; }
+
+        public readonly int Row;
+
+        public readonly int Column;
+
+        public Cell(int x, int y, bool alive)
+        {
+            Column = x;
+            Row = y;
+            Alive = alive;
+        }
+
+
+    }
+}
